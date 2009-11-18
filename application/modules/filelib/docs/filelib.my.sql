2e3c007ce69eb53a793dda77e7db510105274ad4
pekkis <pekkis>
2009-11-18 13:35:55 +0000

    Removados, additores, changetores.

---
+++ application/modules/filelib/docs/filelib.my.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS `filelib_file`;
+DROP TABLE IF EXISTS `filelib_folder`;
+
+CREATE TABLE `filelib_folder` (
+  `id` integer unsigned NOT NULL auto_increment,
+  `parent_id` integer unsigned default NULL,
+  `name` varchar(255) NOT NULL,
+  `created` timestamp NULL,
+  `modified` timestamp NULL default NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `parent_id_name` (`parent_id`,`name`),
+  KEY `parent_id` (`parent_id`),
+  CONSTRAINT `filelib_folder_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `filelib_folder` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+CREATE TABLE `filelib_file` (
+  `id` integer unsigned NOT NULL auto_increment,
+  `folder_id` integer unsigned NOT NULL,
+  `mimetype` varchar(255) NOT NULL,
+  `size` int(11) default NULL,
+  `name` varchar(255) NOT NULL,
+  `iisiurl` text NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `created` timestamp NULL,
+  `modified` timestamp NULL default NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `name` (`name`,`folder_id`),
+  KEY `folder_id` (`folder_id`),
+  KEY `mimetype` (`mimetype`),
+  CONSTRAINT `filelib_file_ibfk_1` FOREIGN KEY (`folder_id`) REFERENCES `filelib_folder` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
---
+++ application/modules/filelib/docs/filelib.postgre.sql
@@ -0,0 +1,35 @@
+CREATE SEQUENCE filelib_folder_id_seq;
+
+CREATE TABLE filelib_folder
+(
+  id integer NOT NULL DEFAULT NEXTVAL('filelib_folder_id_seq'),
+  parent_id integer  default NULL,
+  name varchar(255) NOT NULL,
+  created timestamp NULL,
+  modified timestamp NULL,
+  PRIMARY KEY (id),
+  UNIQUE (parent_id,name),
+  FOREIGN KEY (parent_id) REFERENCES filelib_folder (id) ON DELETE NO ACTION ON UPDATE CASCADE
+);
+
+CREATE SEQUENCE filelib_file_id_seq;
+
+CREATE TABLE filelib_file
+(
+  id integer NOT NULL DEFAULT NEXTVAL('filelib_file_id_seq'),
+  folder_id integer  NOT NULL,
+  mimetype varchar(255) NOT NULL,
+  size integer default NULL,
+  name varchar(255) NOT NULL,
+  iisiurl text NOT NULL,
+  path varchar(255) NOT NULL,
+  created timestamp NULL,
+  modified timestamp NULL,
+  PRIMARY KEY (id),
+  UNIQUE (name,folder_id),
+  FOREIGN KEY (folder_id) REFERENCES filelib_folder (id) ON DELETE NO ACTION ON UPDATE CASCADE
+);
+
+CREATE INDEX mimetype ON filelib_file(mimetype);
+
+
