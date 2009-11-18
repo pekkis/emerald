9b52992d1c1e18af48ae3a5a42eb8fa7055fb49c
pekkis <pekkis>
2009-11-18 11:25:07 +0000

    Added stuff.

---
+++ library/Emerald/Application/Resource/Filelib.php
@@ -0,0 +1,22 @@
+<?php
+class Emerald_Application_Resource_Filelib extends Zend_Application_Resource_ResourceAbstract
+{
+
+	
+	public function init()
+	{
+		
+		die('hello!');
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
