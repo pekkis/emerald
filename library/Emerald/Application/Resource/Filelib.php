83bc9fd2aada4cab6e5985a13c0fd5050f8f692f
Mikko Forsström <puhemies@diktaattoriporssi.com>
2009-11-18 13:55:07 +0000

    Returning filelib object.

--- library/Emerald/Application/Resource/Filelib.php
+++ library/Emerald/Application/Resource/Filelib.php
@@ -3,7 +3,7 @@ class Emerald_Application_Resource_Filelib extends Zend_Application_Resource_Res
 {
 
 	protected $_filelib;
-	
+
 	public function getFilelib()
 	{
 		if(!$this->_filelib) {
@@ -12,30 +12,33 @@ class Emerald_Application_Resource_Filelib extends Zend_Application_Resource_Res
 			if(isset($options['DbResource'])) {
 				$this->getBootstrap()->bootstrap($options['DbResource']);
 				$options['Db'] = $this->getBootstrap()->getResource($options['DbResource']);
-												
+
 				unset($options['DbResource']);
 			}
-			$this->_filelib = new Emerald_Filelib($options);			
+			$this->_filelib = new Emerald_Filelib($options);
 		}
-						
+
 		return $this->_filelib;
 	}
-	
-	
+
+    /**
+     * Init
+     *
+     * @return Emerald_Filelib
+     */
 	public function init()
 	{
 		$filelib = $this->getFilelib();
 
 		Zend_Registry::set('Emerald_Filelib', $filelib);
-		
-		
-		
+
+		return $filelib;
 	}
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
 }
