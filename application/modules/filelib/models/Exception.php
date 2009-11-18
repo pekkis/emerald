96a3964dcf37c50da64347d08e437c81d89e6c06
pekkis <pekkis>
2009-11-18 13:37:00 +0000

    Removados.

--- application/modules/filelib/models/Exception.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Emerald_Filelib_Exception extends Zend_Exception
-{
-	
-	
-	
-	
-}
--- application/modules/filelib/models/Filelib/BadMimeTypeException.php
+++
@@ -1,3 +0,0 @@
-<?php
-class Emerald_Filelib_BadMimeTypeException extends Emerald_Filelib_Exception {}
-?>
