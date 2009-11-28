33b038437c3cddafe7b02602ab47b596c172c325
pekkis <pekkis>
2009-11-28 11:26:11 +0000

    Additores los renderos motores del files con filelibs.

---
+++ application/modules/filelib/controllers/FileController.php
@@ -0,0 +1,35 @@
+<?php
+class Filelib_FileController extends Zend_Controller_Action
+{
+	
+	public function renderAction()
+	{
+		$fl = Zend_Registry::get('Emerald_Filelib');
+		$file = $fl->findFile($this->_getParam('id'));
+		
+		$version = $this->_getParam('version');
+
+		$download = $this->_getParam('download');
+		
+		$opts = array();
+		
+		if($version) {
+			$opts['version'] = $version;
+		}
+
+		if($download) {
+			$opts['download'] = true;
+		}
+				
+		$file->render($this->getResponse(), $opts);
+
+		
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNoRender();
+
+	
+	}	
+	
+	
+	
+}
