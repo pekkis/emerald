d26cbe3d4c502bac24752b0d940ef8da340b660c
pekkis <pekkis>
2009-12-17 21:04:54 +0000

    Added translator container. Removed translator poo from message and moved it to t.c.

--- library/Emerald/Json/Message.php
+++ library/Emerald/Json/Message.php
@@ -13,22 +13,7 @@ class Emerald_Json_Message extends Emerald_Model_AbstractItem
 	const ERROR = 4;
 	
 	
-    /**
-     * @var Zend_Translate
-     */
-    protected $_translator;
-
-    /**
-     * Global default translation adapter
-     * @var Zend_Translate
-     */
-    protected static $_translatorDefault;
-
-    /**
-     * is the translator disabled?
-     * @var bool
-     */
-    protected $_translatorDisabled = false;
+	protected $_translatorContainer = null;
         
     public function __construct($type, $message)
     {
@@ -42,123 +27,32 @@ class Emerald_Json_Message extends Emerald_Model_AbstractItem
     }
     
     
+    public function getTranslatorContainer()
+    {
+    	if(!$this->_translatorContainer) {
+    		$this->_translatorContainer = new Emerald_TranslatorContainer();
+    	}
+    	return $this->_translatorContainer;
+    }
+    
+    
+    public function setTranslationContainer(Emerald_TranslationContainer $translationContainer)
+    {
+    	$this->_translatorContainer = $translationContainer;
+    }
+    
+    
     public function toArray()
     {
     	$arr = parent::toArray();
     	if(isset($arr['message']) && $arr['message']) {
-    		if($translator = $this->getTranslator()) {
-				$arr['message'] = $this->getTranslator()->translate($arr['message']);    			
+    		if($translator = $this->getTranslatorContainer()->getTranslator()) {
+				$arr['message'] = $translator->translate($arr['message']);    			
     		}
     	}
     	return $arr;
     }
-    
-    
-    
-
-    /**
-     * Set translator object
-     *
-     * @param  Zend_Translate|Zend_Translate_Adapter|null $translator
-     * @return Zend_Form
-     */
-    public function setTranslator($translator = null)
-    {
-        if (null === $translator) {
-            $this->_translator = null;
-        } elseif ($translator instanceof Zend_Translate_Adapter) {
-            $this->_translator = $translator;
-        } elseif ($translator instanceof Zend_Translate) {
-            $this->_translator = $translator->getAdapter();
-        } else {
-            require_once 'Zend/Form/Exception.php';
-            throw new Zend_Form_Exception('Invalid translator specified');
-        }
-
-        return $this;
-    }
-
-    /**
-     * Set global default translator object
-     *
-     * @param  Zend_Translate|Zend_Translate_Adapter|null $translator
-     * @return void
-     */
-    public static function setDefaultTranslator($translator = null)
-    {
-        if (null === $translator) {
-            self::$_translatorDefault = null;
-        } elseif ($translator instanceof Zend_Translate_Adapter) {
-            self::$_translatorDefault = $translator;
-        } elseif ($translator instanceof Zend_Translate) {
-            self::$_translatorDefault = $translator->getAdapter();
-        } else {
-            require_once 'Zend/Form/Exception.php';
-            throw new Zend_Form_Exception('Invalid translator specified');
-        }
-    }
-
-    /**
-     * Retrieve translator object
-     *
-     * @return Zend_Translate|null
-     */
-    public function getTranslator()
-    {
-        if ($this->translatorIsDisabled()) {
-            return null;
-        }
-
-        if (null === $this->_translator) {
-            return self::getDefaultTranslator();
-        }
-
-        return $this->_translator;
-    }
-
-    /**
-     * Get global default translator object
-     *
-     * @return null|Zend_Translate
-     */
-    public static function getDefaultTranslator()
-    {
-        if (null === self::$_translatorDefault) {
-            require_once 'Zend/Registry.php';
-            if (Zend_Registry::isRegistered('Zend_Translate')) {
-                $translator = Zend_Registry::get('Zend_Translate');
-                if ($translator instanceof Zend_Translate_Adapter) {
-                    return $translator;
-                } elseif ($translator instanceof Zend_Translate) {
-                    return $translator->getAdapter();
-                }
-            }
-        }
-        return self::$_translatorDefault;
-    }
-
-    /**
-     * Indicate whether or not translation should be disabled
-     *
-     * @param  bool $flag
-     * @return Zend_Form
-     */
-    public function setDisableTranslator($flag)
-    {
-        $this->_translatorDisabled = (bool) $flag;
-        return $this;
-    }
-
-    /**
-     * Is translation disabled?
-     *
-     * @return bool
-     */
-    public function translatorIsDisabled()
-    {
-        return $this->_translatorDisabled;
-    }
-    
+   
     
 }
 ?>
---
+++ library/Emerald/TranslatorContainer.php
@@ -0,0 +1,128 @@
+<?php
+class Emerald_TranslatorContainer
+{
+	/**
+     * @var Zend_Translate
+     */
+    protected $_translator;
+
+    /**
+     * Global default translation adapter
+     * @var Zend_Translate
+     */
+    protected static $_translatorDefault;
+
+    /**
+     * is the translator disabled?
+     * @var bool
+     */
+    protected $_translatorDisabled = false;
+	
+	
+        
+
+    /**
+     * Set translator object
+     *
+     * @param  Zend_Translate|Zend_Translate_Adapter|null $translator
+     * @return Zend_Form
+     */
+    public function setTranslator($translator = null)
+    {
+        if (null === $translator) {
+            $this->_translator = null;
+        } elseif ($translator instanceof Zend_Translate_Adapter) {
+            $this->_translator = $translator;
+        } elseif ($translator instanceof Zend_Translate) {
+            $this->_translator = $translator->getAdapter();
+        } else {
+            require_once 'Zend/Form/Exception.php';
+            throw new Zend_Form_Exception('Invalid translator specified');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set global default translator object
+     *
+     * @param  Zend_Translate|Zend_Translate_Adapter|null $translator
+     * @return void
+     */
+    public static function setDefaultTranslator($translator = null)
+    {
+        if (null === $translator) {
+            self::$_translatorDefault = null;
+        } elseif ($translator instanceof Zend_Translate_Adapter) {
+            self::$_translatorDefault = $translator;
+        } elseif ($translator instanceof Zend_Translate) {
+            self::$_translatorDefault = $translator->getAdapter();
+        } else {
+            require_once 'Zend/Form/Exception.php';
+            throw new Zend_Form_Exception('Invalid translator specified');
+        }
+    }
+
+    /**
+     * Retrieve translator object
+     *
+     * @return Zend_Translate|null
+     */
+    public function getTranslator()
+    {
+        if ($this->translatorIsDisabled()) {
+            return null;
+        }
+
+        if (null === $this->_translator) {
+            return self::getDefaultTranslator();
+        }
+
+        return $this->_translator;
+    }
+
+    /**
+     * Get global default translator object
+     *
+     * @return null|Zend_Translate
+     */
+    public static function getDefaultTranslator()
+    {
+        if (null === self::$_translatorDefault) {
+            require_once 'Zend/Registry.php';
+            if (Zend_Registry::isRegistered('Zend_Translate')) {
+                $translator = Zend_Registry::get('Zend_Translate');
+                if ($translator instanceof Zend_Translate_Adapter) {
+                    return $translator;
+                } elseif ($translator instanceof Zend_Translate) {
+                    return $translator->getAdapter();
+                }
+            }
+        }
+        return self::$_translatorDefault;
+    }
+
+    /**
+     * Indicate whether or not translation should be disabled
+     *
+     * @param  bool $flag
+     * @return Zend_Form
+     */
+    public function setDisableTranslator($flag)
+    {
+        $this->_translatorDisabled = (bool) $flag;
+        return $this;
+    }
+
+    /**
+     * Is translation disabled?
+     *
+     * @return bool
+     */
+    public function translatorIsDisabled()
+    {
+        return $this->_translatorDisabled;
+    }
+ 
+    
+}
