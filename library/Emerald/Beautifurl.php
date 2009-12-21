cd738df17fe84721f3e8783d4394f13187a8cea5
dee <dee>
2009-12-21 10:30:18 +0000

    accept a beautifier character/string

--- library/Emerald/Beautifurl.php
+++ library/Emerald/Beautifurl.php
@@ -1,24 +1,30 @@
 <?php
 class Emerald_Beautifurl
 {
-	
-	public static function fromString($str)
+	/**
+	 * Make a beautifurl from a string
+	 * 
+	 * @param string $str Source string
+	 * @param string $beautifier String to use for spacing
+	 * @return string
+	 */
+	public static function fromString($str, $beautifier = '_')
 	{
 		$beautifurl = htmlentities(mb_strtolower($str, 'utf8'), ENT_COMPAT, 'utf-8');
 		$beautifurl = preg_replace("/&(.)(acute|cedil|circ|ring|tilde|uml);/", "$1", $beautifurl);
-		$beautifurl = preg_replace("/([^a-z0-9]+)/", "_", html_entity_decode($beautifurl));
-    	$beautifurl = trim($beautifurl, "_");
+		$beautifurl = preg_replace("/([^a-z0-9]+)/", $beautifier, html_entity_decode($beautifurl));
+    	$beautifurl = trim($beautifurl, $beautifier);
 
     	return $beautifurl;
 	}
 	
 	
 	
-	public static function fromArray(array $fragments, $prepend = null)
+	public static function fromArray(array $fragments, $prepend = null, $beautifier = '_')
 	{
 		$beautifulFragments = array();
 		foreach($fragments as $fragment) {
-			$beautifulFragments[] = self::fromString($fragment);
+			$beautifulFragments[] = self::fromString($fragment, $beautifier);
 		}
 
 		if($prepend) {
@@ -29,10 +35,4 @@ class Emerald_Beautifurl
 		return $beautifurl;
 		
 	}
-	
-	
-	
-	
-	
-	
 }
