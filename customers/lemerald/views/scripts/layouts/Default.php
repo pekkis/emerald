5b5d6bd9a7b6853a319b4a503bbf3bc1ceabe63d
pekkis <pekkis>
2009-12-27 21:06:39 +0000

    Second customer for hardcore testing of ideas.

---
+++ customers/lemerald/views/scripts/layouts/Default.php
@@ -0,0 +1,26 @@
+<?php 
+class Emerald_Layout_Default extends Emerald_Layout
+{
+	
+	
+	protected function _run()
+	{
+		// $this->actionToStack('index', 'html-content', 'emerald', array('page' => $this->getPage(), 'block_id' => 2, 'rs' => 'sidebar_one'));
+		
+		// $this->actionToStack('index', 'html-content', 'emerald', array('page' => $this->getPage(), 'block_id' => 3, 'rs' => 'sidebar_two'));
+				
+		$this->actionToStack('index', 'menu', 'core', array('page' => $this->getPage(), 'extended' => 1, 'rs' => 'navi'));
+		
+		$this->shard($this->getPage(), $this->getPage()->shard_id, array('block_id' => 1));
+		
+		
+	}
+	
+	protected function _runAjax()
+	{
+		$this->shard($this->getPage(), $this->getPage()->shard_id, array('block_id' => 1));
+	}
+	
+	
+	
+}
