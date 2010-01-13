1576af024c4473e000023ddd51e96a7cbf6c6590
pekkis <pekkis>
2010-01-13 12:14:00 +0000

    Fixores.

--- library/Emerald/Controller/Plugin/Common.php
+++ library/Emerald/Controller/Plugin/Common.php
@@ -31,12 +31,12 @@ class Emerald_Controller_Plugin_Common extends Zend_Controller_Plugin_Abstract
 				$page->setPrivilege('read');
 			}
 			
-			Zend_Controller_Front::getInstance()->registerPlugin(new Emerald_Controller_Plugin_Acl($acl, $user));
+			$aclPlugin = new Emerald_Controller_Plugin_Acl($acl, $user);
 			
-			if($user->inGroup(Core_Model_Group::GROUP_ANONYMOUS))
-			{
-				$this->getResponse()->setRedirect('/login');
-			}
+			$aclPlugin->setErrorPage('index', 'login', 'default');
+			
+						
+			Zend_Controller_Front::getInstance()->registerPlugin($aclPlugin);
 			
 			// $this->view->translate()->setTranslator(Zend_Registry::get('Zend_Translate'));
 			// $this->view->translate()->setLocale(Zend_Registry::get('Zend_Locale') ? Zend_Registry::get('Zend_Locale') : 'en');
