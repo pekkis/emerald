177abcd55ebdcaf26b9e6ae8592c221e7921040b
pekkis <pekkis>
2010-02-22 10:10:12 +0000

    Return of cache manager.

---
+++ library/Emerald/Cache/Manager.php
@@ -0,0 +1,36 @@
+<?php
+class Emerald_Cache_Manager implements IteratorAggregate
+{
+	
+	
+	public function getIterator()
+	{
+		return new ArrayIterator($this->_caches);
+	}
+	
+	
+	private $_caches = array();
+	
+	
+	
+	public function __construct()
+	{
+		
+	}
+	
+	
+	
+	public function setCache($name, Zend_Cache_Core $cache)
+	{
+		$this->_caches[$name] = $cache;
+	}
+	
+	
+	public function getCache($name)
+	{
+		return $this->_caches[$name];
+	}
+		
+	
+	
+}
