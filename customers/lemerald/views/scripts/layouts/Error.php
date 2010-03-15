36a3e42b27a509ed7fdca3b41da19bbe0e0b9d07
pekkis <pekkis>
2010-03-15 19:59:47 +0000

    Added lemerald for now...

---
+++ customers/lemerald/views/scripts/layouts/Error.php
@@ -0,0 +1,33 @@
+<?php 
+class Emerald_Layout_Error extends Emerald_Layout
+{
+	
+	protected function _init()
+	{
+		$this->setLayoutFile('error');
+	}
+	
+	
+	protected function _run()
+	{
+		// $this->actionToStack('index', 'html-content', 'emerald', array('page' => $this->getPage(), 'block_id' => 2, 'rs' => 'sidebar_one'));
+		
+		// $this->actionToStack('index', 'html-content', 'emerald', array('page' => $this->getPage(), 'block_id' => 3, 'rs' => 'sidebar_two'));
+				
+		$this->actionToStack('index', 'menu', 'core', array('page' => $this->getPage(), 'extended' => 1, 'rs' => 'navi'));
+		
+		// $this->shard($this->getPage(), $this->getPage()->shard_id, array('block_id' => 1));
+		
+		
+		
+	}
+	
+	protected function _runAjax()
+	{
+		
+		// $this->shard($this->getPage(), $this->getPage()->shard_id, array('block_id' => 1));
+	}
+	
+	
+	
+}
