37dc1d10cb3b1ed2b87132dd9daf373c6ef18945
pekkis <pekkis>
2010-03-25 18:45:52 +0000

    Added interlinking stuff to create too.

--- public/lib/admin/page/create.js
+++ public/lib/admin/page/create.js
@@ -1,7 +1,64 @@
+String.prototype.repeat = function(l){
+	return new Array(l+1).join(this);
+};
+
+
+naviUpdate = function(pages, level, navitree) {
+			
+	$.each(navitree, function(key, navi) {
+		
+		if(navi.id) {
+			pages.push({ key: navi.id, value: "-".repeat(level) + navi.label});
+		}
+		
+		
+		if(navi.pages) {
+			var lev2 = level++;
+			naviUpdate(pages, lev2, navi.pages);
+		}
+		
+	});
+};
+
 $(document).ready(function() {
+		
 	$('form').jsonSubmit({
 		success: function(msg) {
 			location = Emerald.baseUrl + '/admin/page/edit/id/' + msg.page_id;
 		}
 	});
+	
+	$('#interlink_page').change(function() {
+		
+		if(this.value) {
+			$("#global_id").val(this.value);
+		}
+		
+	});
+	
+	
+	$('#interlink_locale').change(function() {
+		
+		var $this = $(this);
+						
+		$.getJSON("/menu/index", { 'locale': this.value, 'format' : 'json' }, function(r) {
+		
+			var pages = [];
+			
+			naviUpdate(pages, 1, r.pages);
+						
+			$("#interlink_page").empty().append($('<option value="">--</option>'));
+			
+			$.each(pages, function(key, value) {
+				$("#interlink_page").append($('<option value="' + value.key + '">' + value.value + '</option>'));
+			});
+			
+			
+		});
+		
+	
+	});
+	
+
 });
+
