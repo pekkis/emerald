bcb2c4adaa7609118bbfce2257797962f3f076dc
pekkis <pekkis>
2010-06-11 10:45:16 +0000

    Update filelib example confs.

--- application/modules/em-filelib/docs/filelib.my.sql
+++ application/modules/em-filelib/docs/filelib.my.sql
@@ -1,34 +1,28 @@
-DROP TABLE IF EXISTS `filelib_file`;
-DROP TABLE IF EXISTS `filelib_folder`;
-
-CREATE TABLE `filelib_folder` (
-  `id` integer unsigned NOT NULL auto_increment,
-  `parent_id` integer unsigned default NULL,
+CREATE TABLE emerald_filelib_folder (
+  id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  parent_id int(10) unsigned DEFAULT NULL,
   `name` varchar(255) NOT NULL,
-  `created` timestamp NULL,
-  `modified` timestamp NULL default NULL,
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `parent_id_name` (`parent_id`,`name`),
-  KEY `parent_id` (`parent_id`),
-  CONSTRAINT `filelib_folder_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `filelib_folder` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+  visible tinyint unsigned NOT NULL default 1,
+  PRIMARY KEY (id),
+  UNIQUE KEY parent_id_name (parent_id,`name`),
+  KEY parent_id (parent_id),
+  CONSTRAINT filelib_folder_ibfk_1 FOREIGN KEY (parent_id) REFERENCES emerald_filelib_folder (id) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-CREATE TABLE `filelib_file` (
-  `id` integer unsigned NOT NULL auto_increment,
-  `folder_id` integer unsigned NOT NULL,
-  `mimetype` varchar(255) NOT NULL,
-  `size` int(11) default NULL,
+CREATE TABLE emerald_filelib_file (
+  id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  folder_id int(10) unsigned NOT NULL,
+  mimetype varchar(255) NOT NULL,
+  profile varchar(255) NOT NULL DEFAULT 'default',
+  size int(11) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
-  `link` varchar(1000) NULL,
-  `path` varchar(255) NOT NULL,
-  `created` timestamp NULL,
-  `modified` timestamp NULL default NULL,
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `name` (`name`,`folder_id`),
-  KEY `folder_id` (`folder_id`),
-  KEY `mimetype` (`mimetype`),
-  CONSTRAINT `filelib_file_ibfk_1` FOREIGN KEY (`folder_id`) REFERENCES `filelib_folder` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+  link varchar(1000) DEFAULT NULL,
+  path varchar(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY `name` (`name`,folder_id),
+  KEY folder_id (folder_id),
+  KEY mimetype (mimetype),
+  CONSTRAINT filelib_file_ibfk_1 FOREIGN KEY (folder_id) REFERENCES emerald_filelib_folder (id) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
--- application/modules/em-filelib/docs/filelib.postgre.sql
+++ application/modules/em-filelib/docs/filelib.postgre.sql
@@ -1,35 +1,26 @@
-CREATE SEQUENCE filelib_folder_id_seq;
+CREATE SEQUENCE emerald_filelib_folder_id_seq;
 
-CREATE TABLE filelib_folder
-(
-  id integer NOT NULL DEFAULT NEXTVAL('filelib_folder_id_seq'),
-  parent_id integer  default NULL,
-  name varchar(255) NOT NULL,
-  created timestamp NULL,
-  modified timestamp NULL,
-  PRIMARY KEY (id),
-  UNIQUE (parent_id,name),
-  FOREIGN KEY (parent_id) REFERENCES filelib_folder (id) ON DELETE NO ACTION ON UPDATE CASCADE
+CREATE TABLE "emerald_filelib_folder" (
+  "id" int  NOT NULL DEFAULT NEXTVAL('emerald_filelib_folder_id_seq'),
+  "parent_id" int  DEFAULT NULL,
+  "name" varchar(255) NOT NULL,
+  "visible" smallint NOT NULL default 1,
+  PRIMARY KEY ("id"),
+  UNIQUE ("parent_id","name"),
+    FOREIGN KEY ("parent_id") REFERENCES "emerald_filelib_folder" ("id") ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
-CREATE SEQUENCE filelib_file_id_seq;
+CREATE SEQUENCE emerald_filelib_file_id_seq;
 
-CREATE TABLE filelib_file
-(
-  id integer NOT NULL DEFAULT NEXTVAL('filelib_file_id_seq'),
-  folder_id integer  NOT NULL,
-  mimetype varchar(255) NOT NULL,
-  size integer default NULL,
-  name varchar(255) NOT NULL,
-  link varchar(1000) NULL,
-  path varchar(255) NOT NULL,
-  created timestamp NULL,
-  modified timestamp NULL,
-  PRIMARY KEY (id),
-  UNIQUE (name,folder_id),
-  FOREIGN KEY (folder_id) REFERENCES filelib_folder (id) ON DELETE NO ACTION ON UPDATE CASCADE
+CREATE TABLE "emerald_filelib_file" (
+  "id" int  NOT NULL DEFAULT NEXTVAL('emerald_filelib_file_id_seq'),
+  "folder_id" int  NOT NULL,
+  "mimetype" varchar(255) NOT NULL,
+  "profile" varchar(255) NOT NULL DEFAULT 'default',
+  "size" int DEFAULT NULL,
+  "name" varchar(255) NOT NULL,
+  "link" varchar(1000) DEFAULT NULL,
+  PRIMARY KEY ("id"),
+  UNIQUE ("name","folder_id"),
+      FOREIGN KEY ("folder_id") REFERENCES "emerald_filelib_folder" ("id") ON DELETE NO ACTION ON UPDATE CASCADE
 );
-
-CREATE INDEX mimetype ON filelib_file(mimetype);
-
-
