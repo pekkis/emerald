91ae51eef58b7315675839662c246f30f7940f3c
pekkis <pekkis>
2010-08-12 09:41:38 +0000

    Fixed script to work.

--- application/modules/em-filelib/scripts/jobs/mass-recreate-versions.php
+++ application/modules/em-filelib/scripts/jobs/mass-recreate-versions.php
@@ -1,93 +1,111 @@
 <?php
+ob_start();
+
 set_include_path(realpath(dirname(__FILE__) . '/../../../../../library'));
 
 // Define path to application directory
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH',
-              realpath(dirname(__FILE__) . '/../../../../../application'));
+|| define('APPLICATION_PATH',
+realpath(dirname(__FILE__) . '/../../../../../application'));
 
 // Define application environment
 defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV',
-              (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV')
-                                         : 'production'));
+|| define('APPLICATION_ENV',
+(getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV')
+: 'production'));
+
+require_once "Zend/Console/Getopt.php";
+require_once "Zend/Debug.php";
+
+$opts = new Zend_Console_Getopt(
+array(
+        'ini|i=s' => 'ini file name(f.ex application.ini)',
+        'env|e=s' => 'env variables',
+        'bootstrap|b=s' => 'Bootstrap sequence, use \'auto\' for automatic',
+        'help|h' => 'Help!',
+        'symlinks|s' => 'Recreate symlinks',
+)
+);
+
+try {
+	$opts->parse();
+	if ($opts->getOption('h')) {
+		die( $opts->getUsageMessage() );
+	}
+} catch (Zend_Console_Getopt_Exception $e) {
+	die( $opts->getUsageMessage() );
+}
+
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
-    APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
+APPLICATION_ENV,
+APPLICATION_PATH . '/configs/' . $opts->ini
 );
 
 
-$opts = new Zend_Console_Getopt(
-	array(
-		'help|h' => 'Help!',
-		'before|b=s' => 'Bootstrap before filelib, ; as separator',
-		'after|a=s' => 'Bootstrap after filelib, ; as separator',
-		'symlinks|s' => 'Recreate symlinks',
-	)
-);
+$env = $opts->env;
 
-try {
-	$opts->parse();
-	if ($opts->getOption('h')) {
-   		die( $opts->getUsageMessage() );
+if($env) {
+	$env = explode(',', $env);
+	foreach($env as $e) {
+		putenv($e);
 	}
-} catch (Zend_Console_Getopt_Exception $e) {
-   	die( $opts->getUsageMessage() );
 }
 
-$before = explode(";", $opts->b);
 
-$after = explode(";", $opts->a);
 
-foreach($before as $b) {
-	$application->bootstrap($b);
-}
+$bootstraps = explode(",", $opts->bootstrap);
 
-$application->bootstrap('db');
 
-foreach($after as $a) {
-	$application->bootstrap($a);
+foreach($bootstraps as $b) {
+    
+	if($b == 'auto') {
+		$application->bootstrap();
+	   break;	
+	} else {
+		$application->bootstrap($b);
+	}
+
 }
 
 $recreateSymlinks = $opts->s;
 
-
+/** @var Emerald_Filelib */
 $filelib = Zend_Registry::get('Emerald_Filelib');
 
-$files = $filelib->findAllFiles();
+$files = $filelib->file()->findAll();
 
 echo "\n\n";
 
 foreach($files as $file) {
-	
-	echo "Recreate versions for #{$file->id}, of mimetype '{$file->mimetype}' and type '{$file->getType()}'\n";
-			
-	foreach($filelib->getPlugins() as $plugin) {
+
+	echo "Recreate versions for #{$file->id} ('{$file->name}'), mime '{$file->mimetype}',type '{$file->getType()}', profile '{$file->profile}'\n";
 		
-		if($plugin instanceof Emerald_Filelib_Plugin_VersionProvider_Interface && $plugin->providesFor($file)) {
+	foreach($filelib->getPlugins() as $plugin) {
+
+		if($plugin instanceof Emerald_Filelib_Plugin_VersionProvider_Interface && $filelib->file()->hasVersion($file, $plugin->getIdentifier())) {
 
 			try {
 				echo "\tProcessing version '{$plugin->getIdentifier()}' => ";
 				$plugin->createVersion($file);
 				echo "Great success!";
 			} catch(Exception $e) {
-				echo "EPIC FAIL!";
-			}		
+				echo "EPIC FAIL with {$e->getMessage()}";
+			}
 			echo "\n";
-		
+
 		}
-		
+
 		if($recreateSymlinks) {
 			$filelib->getSymlinker()->deleteSymlink($file);
 			$filelib->getSymlinker()->createSymlink($file);
 		}
 	}
-	
+
 	echo "\n";
 }
 
